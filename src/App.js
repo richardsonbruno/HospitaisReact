632f3c4ff27314fa24b876fad52f3739d8cf91bc
Richardson Bruno <richardsonbruno@pop-os.localdomain>
2021-02-25 21:07:21 -0300

    melhorar a performance

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import GlobalStyle from './global/style';
 import { HospitalContext } from './context/HospitalContext';
 
 // Components
-import SearchBar from './components/SearchBar';
 import HospitalList from './components/HospitalList';
 
 function App() {
   return (
     <div className="container">
       <HospitalContext>
-        <SearchBar />
         <HospitalList />
         <GlobalStyle />
       </HospitalContext>
